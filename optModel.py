3d0ed15192d1daa8cc6b227bc8cf3da0d5606b1e
Hiyong <bhy2pencil@aim.com>
2016-03-15 15:50:48 -0400

    Display fix

--- optModel.py
+++ optModel.py
@@ -46,7 +46,7 @@ with open("data/kStarting.csv","rb") as csvfile:
  
 
 
-source = [indexDict["Kiambi"]]
+source = [indexDict["Nyunzu"]]
 sink =[indexDict["Lubumbashi"]]
 itm = [source[0],sink[0]]
 season = "d"
@@ -70,10 +70,62 @@ st([(x[i,j])==0 for i in sink for j in jid if i!=j])
 #conservation of flow
 st([sum( x[i,j] for i in iid if i!=j)== sum( x[j,i] for i in iid if i!=j) for j in jid if j not in itm])
 
-solve("float")
-print("Total Cost= %g"%(vobj()))
+solve()
+print("Total Cost = %g"%(vobj()))
+
+routeList = []
 
 for item in E:
 	if x[item].primal > 0:
-		print x[item]
-		print temp[item[0]],temp[item[1]]
+	 	routeList.append(item)
+
+print "\nOriginal order:"
+print(routeList)
+
+sortedRoute = []
+
+#the index of the node containing the origin
+oindex = [sr[0] for sr in routeList].index(source[0])
+
+#add the first node to the sortedRoute list
+sortedRoute.append(routeList[oindex])
+
+index = 0
+#go until j of the last node in the sortedRoute list is equal to the destination; maybe add something later so that it removes the item from routeList, so it's easier to find index of item?
+while sortedRoute[index][1] != sink[0]:
+	loc = [sr[0] for sr in routeList].index(sortedRoute[index][1])
+	sortedRoute.append(routeList[loc])
+	index = index + 1
+	
+print "\nOrdered:"
+print(sortedRoute)
+print "\n"
+for i in sortedRoute:
+	print temp[i[0]],"-->",temp[i[1]],": ",distm[i[0]][i[1]]
+
+brevRoute = []
+start = 0
+end = 0
+
+while start < len(sortedRoute):
+	distance = 0
+	while "-" in temp[sortedRoute[end][1]]:
+		distance = distance + distm[sortedRoute[end][0]][sortedRoute[end][1]]
+		end = end + 1
+	distance = distance + distm[sortedRoute[end][0]][sortedRoute[end][1]]
+
+	brevRoute.append((sortedRoute[start][0],sortedRoute[end][1],distance))
+
+	start = end + 1
+	end = end + 1
+
+print "\nAbbreviated:"
+print(brevRoute)
+print "\n"
+for i in brevRoute:
+	print temp[i[0]],"-->",temp[i[1]],": ",i[2]
+
+			
+			
+			
+			
